fdc94f9fef54347b7c60828e3b3a41488ae36681
JoynulIslam <joynul8763@gmail.com>
2025-02-15 20:12:36 -0800

    modify2

--- events/urls.py
+++ events/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from events.views import home,dashboard,event_detail,add_event,add_category,add_participant,delete_event,update_event
 
 urlpatterns = [
-    path(' ',home , name='home'),
+    path('home/',home , name='home'),
     path('dashboard_o/', dashboard, name='dashboard_o'),
      path("event/<int:id>/", event_detail, name="event-detail"),
     path("event/add/", add_event, name="add_event"),
